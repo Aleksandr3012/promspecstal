3f45dc494585e375c706a48003a265bb5482d4a7
Pavel <wol1414@gmail.com>
2021-03-31 12:45:55 +0300

    переделал на sticky

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -94,12 +94,12 @@ function eventHandler() {
 
 	// console.log('.content-left');
 
-	var Sticky = new hcSticky('.sidebar-fixed', {
-		// stickTo: 'main'
-		stickTo: $('main')[0],
-		// innerTop: 50,
-		bottomEnd: 500
-	});
+	// var Sticky = new hcSticky('.sidebar-fixed', {
+	// 	// stickTo: 'main'
+	// 	stickTo: $('main')[0],
+	// 	// innerTop: 50,
+	// 	bottomEnd: 500
+	// });
 
 
 };
